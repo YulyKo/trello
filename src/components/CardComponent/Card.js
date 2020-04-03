bb5917ac7712ec95ed4e6aac6f8317210f3327b8
YulyKo <juliakotenko.forstudy@gmail.com>
2020-04-03 07:11:28 +0300

    adding card done

---
+++ src/components/CardComponent/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./_Card.css";
+
+export default class Card extends React.Component{
+  render() {
+    return (
+      <article className="card">
+        <input className="card_name-changer" type="text" name="" id=""/>
+        <p className="card_name">name of card</p>
+      </article>
+    );
+  }
+}
--- src/components/ColumnComponent/Column.js
+++ src/components/ColumnComponent/Column.js
@@ -1,19 +1,26 @@
 import * as React from 'react';
 import "./_Column.css"
+import {useState} from "react";
+import Card from "../CardComponent/Card";
 // __ - block
 // _  - elem
 // -- - modifier
 
-class Column extends React.Component {
-  render() {
-    return (
-      <section className="column__container">
-        <input className="column__container_name-changer" type="text"/>
-        <p>name of column</p>
-        <button className="column__container_add-card-btn">Add card</button>
-      </section>
-    );
-  }
+ const Column = props => {
+  const [LIST, SET_LIST] = useState([]);
+
+  const addCard = () => {
+    SET_LIST(LIST.concat(<Card key={LIST.length}/>))
+  };
+
+  return (
+    <section className="column__container">
+      <input className="column__container_name-changer" type="text"/>
+      <p>name of column</p>
+      {LIST}
+      <button className="column__container_add-card-btn" onClick={addCard}>Add card</button>
+    </section>
+  );
 }
 
 export default Column;
